6e30420133cf3a7e5058a5dc90c293f75570518e
Nick <nick@honestbuildings.com>
2016-09-15 17:03:38 -0400

    adding it to index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'no-inner-compare': require('./lib/rules/no-inner-compare'),
     'missing-assertion': require('./lib/rules/missing-assertion'),
-    'terminating-properties': require('./lib/rules/terminating-properties')
+    'terminating-properties': require('./lib/rules/terminating-properties'),
+    'no-length-assertions': require('./lib/rules/no-length-assertions')
   }
 };

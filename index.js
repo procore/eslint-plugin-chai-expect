2db4b9e7b8c96dadd58b7f636f03a87ffeac5758
Nick <nick@honestbuildings.com>
2016-09-29 10:34:30 -0400

    Adding the linter so that you cannot commit parallel only tests >.<

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
     'no-inner-compare': require('./lib/rules/no-inner-compare'),
     'missing-assertion': require('./lib/rules/missing-assertion'),
     'terminating-properties': require('./lib/rules/terminating-properties'),
-    'no-length-assertions': require('./lib/rules/no-length-assertions')
+    'no-length-assertions': require('./lib/rules/no-length-assertions'),
+    'no-exclusive-parallel': require('./lib/rules/no-exclusive-parallel')
   }
 };
